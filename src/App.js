eb9cd8b06199635ce0a2b86d6835f7ea15cc8235
ftailorr <88861331+ftailorr@users.noreply.github.com>
2022-08-12 15:43:03 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import { Box } from "@mui/material";
+import ExerciseDetail from "./pages/ExerciseDetail";
+import Home from "./pages/Home";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+
+const App = () => {
+  return (
+    <Box width="400px" sx={{ width: { x1: "1488pxx" } }} m="auto">
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercise/:id" element={<ExerciseDetail />} />
+      </Routes>
+      <Footer />
+    </Box>
+  );
+};
+
+export default App;
